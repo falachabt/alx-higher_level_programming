9d529d1e9005cc0b6266abac13a814b4391b5181
falachabt <bennytenezeu@gmail.com>
2023-12-03 13:43:09 -0800

    update import error

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,11 @@
+# 0-add.py
+
 #!/usr/bin/python3
 from add_0 import add
 
-a = 1
-b = 2
-result = add(a, b)
-
-print("{} + {} = {}".format(a, b, result))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
 
